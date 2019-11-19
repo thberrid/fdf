66a09dd20ace0a8ae289c2b83bb830026deef033
Thibault BERRIDO <thberrid@e1r2p21.42.fr>
2019-11-19 10:06:41 +0100

    add keycodes basics

---
+++ includes/keycodes.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keycodes.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/19 03:36:09 by thberrid          #+#    #+#             */
+/*   Updated: 2019/11/19 03:45:56 by thberrid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEYCODES_H
+# define KEYCODES_H
+
+# define KEY_ESC 	53
+# define KEY_ENTER 	36
+
+# define KEY_LEFT	123
+# define KEY_RIGHT	124
+# define KEY_TOP 	126
+# define KEY_BOTTOM	125
+
+# define KEY_SPACE	49
+# define KEY_PLUS	69
+# define KEY_MINUS	78
+
+#endif
